785af012470a28058ddeed5585fec1289830426f
Zhuan Akbar <zhuan.akb@gmail.com>
2021-01-23 17:43:25 +0700

    Inject dependencies product service

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,16 @@ use App\Services\ProductService;
 
 class ProductController extends Controller
 {
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
     public function indexProduct()
     {
-        $resultIndex = (new ProductService())->getAllProduct();
+        $resultIndex = $this->productService->getAllProduct();
         return view('product-list', compact('resultIndex'));
     }
 }
